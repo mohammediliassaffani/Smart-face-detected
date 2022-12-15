af03cfb2dc8083f312b188a82a7826d2e52a75ab
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-15 22:03:56 +0100

    set-up-compenets

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        {/* <Logo />
+    <ImageLinkedForm />
+    <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
