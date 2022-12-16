83ae19927d44e88f21597a445052530a5a1aa5f2
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-16 22:31:27 +0100

    search-btn

--- src/Components/ImageLinkedForm/ImageLinkedForm.jsx
+++ src/Components/ImageLinkedForm/ImageLinkedForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const ImageLinkedForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p className="f3">{'this is magic'}</p>
+      <div>
+        <input className="f4 pa2 w-80 center" type="text" />
+        <button className="w-40 grow f4 link ph3 pv2 dib white bg-light-purple ">detect</button>
+      </div>
+    </div>
+  );
 };
 
 export default ImageLinkedForm;
