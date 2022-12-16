f85d593eb0f1bb3464f0703b6152e970b71cde46
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-16 23:15:16 +0100

    bg-IMG-LInk

--- src/Components/ImageLinkedForm/ImageLinkedForm.jsx
+++ src/Components/ImageLinkedForm/ImageLinkedForm.jsx
@@ -4,10 +4,12 @@ import './ImageLinkedForm.css';
 const ImageLinkedForm = () => {
   return (
     <div>
-      <p className="f3">{' this is magic'}</p>
+      <p className="f3 center">{' this is magic'}</p>
       <div className="center">
-        <input className="f4 pa2 w-70 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple ">detect</button>
+        <div className=" center form pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="text" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple ">detect</button>
+        </div>
       </div>
     </div>
   );
