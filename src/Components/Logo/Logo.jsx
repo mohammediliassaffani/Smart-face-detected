93d86d8d58f358adb0bb2761a019c512049d47c3
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-16 21:23:20 +0100

    adjust-logo

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -5,12 +5,9 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt
-        className="Tilt br2 shadow-2"
-        options={{ max: 25, speed: 300 }}
-        style={{ height: 250, width: 250 }}>
-        <div className="Tilt-inner pa5">
-          <img style={{ paddingTop: '4px', height: '8rem' }} src={brain} alt="Logo" />
+      <Tilt className="Tilt br2 shadow-2" options={{ max: 25, speed: 300 }}>
+        <div className="Tilt-inner ">
+          <img className="Logo" src={brain} alt="Logo" />
         </div>
       </Tilt>
     </div>
