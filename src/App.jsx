a34614be41c0ec7fb6a30f0d28b7eadb9c44a160
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-20 17:55:36 +0100

    add-api-key

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkedForm from './Components/ImageLinkedForm/ImageLinkedForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '3dd2e7bcc29f4e918eaeb9209eebc5b0'
+});
 
 class App extends Component {
   constructor() {
