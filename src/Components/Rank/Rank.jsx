79a5b15938058ce2a67265aecab6c8921239fc53
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-20 14:16:22 +0100

    Add-react-particles-js

--- src/Components/Rank/Rank.jsx
+++ src/Components/Rank/Rank.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Rank = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="white f3">{'iliass ur ranck is'}</div>
+      <div className="white f1">{'#5'}</div>
+    </div>
+  );
 };
 
 export default Rank;
