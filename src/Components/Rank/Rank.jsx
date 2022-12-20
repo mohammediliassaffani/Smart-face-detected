0f3618f4efe9d384a6e9d638ba98adb55c5f4c07
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2022-12-20 14:46:54 +0100

    style-Rank

--- src/Components/Rank/Rank.jsx
+++ src/Components/Rank/Rank.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Rank = () => {
   return (
     <div>
-      <div className="white f3">{'iliass ur ranck is'}</div>
-      <div className="white f1">{'#5'}</div>
+      <div className="center form pa2 br3 shadow-2 white f3">{'iliass ur ranck is'}</div>
+      <div className=" f4 pa2 w-70 center">{'#5'}</div>
     </div>
   );
 };
