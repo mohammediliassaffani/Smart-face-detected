8bf63897416232aa537950871042ee4c308f828f
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-05 20:34:40 +0100

    Clarifai-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Clarifai from 'clarifai';
+
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkedForm from './Components/ImageLinkedForm/ImageLinkedForm';
@@ -9,9 +9,9 @@ import './App.css';
 
 // const Clarifai = require('clarifai');
 
-const app = new Clarifai.App({
-  apiKey: '3dd2e7bcc29f4e918eaeb9209eebc5b0'
-});
+// const app = new Clarifai.App({
+//   apiKey: '3dd2e7bcc29f4e918eaeb9209eebc5b0'
+// });
 
 class App extends Component {
   constructor() {
