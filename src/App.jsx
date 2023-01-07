01997b5644b9981786f2b5d8402ae09ba641de23
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-07 14:36:11 +0100

    clairifai-add-Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import Clarifai from 'clarifai';
 import Logo from './Components/Logo/Logo';
 import ImageLinkedForm from './Components/ImageLinkedForm/ImageLinkedForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Rank from './Components/Rank/Rank';
 import './App.css';
 
-// const Clarifai = require('clarifai');
-
-// const app = new Clarifai.App({
-//   apiKey: ''
-// });
+const app = new Clarifai.App({
+  apiKey: '26923c3cb6214ee3891d65311018e723'
+});
 
 class App extends Component {
   constructor() {
